7684c68637973e997b97ad10b4a83a8702a9767c
Karthikeyan Kuppuraj <42235025+karthikeyan0808@users.noreply.github.com>
2025-09-13 14:47:23 +0530

    Add files via upload
    Week 1 - Home Assignments

---
+++ homeAssignments/week1/Chrome.java
@@ -0,0 +1,36 @@
+package homeAssignments.week1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		
+		// Variable declaration - 1 word - Lower case
+		
+		String browserDeveloper = "Google";
+				
+		int versionReleaseYear = 2025;
+		
+		float chromeVersion = 140.128f;
+		
+	    boolean isbetaVersion = false;
+	    
+	    char shortcutKey = 'C';
+		 
+			
+		System.out.println("The Browser details:\n" + "The name of the browser:"+ browserDeveloper+ "\n" + 
+		"Release year of the browser:"+ versionReleaseYear + "\n" +
+		"Version number of the browser:" + chromeVersion + "\n" +
+		"Is the browser released a Beta version:" + isbetaVersion + "\n" +
+		"The shortcut to launch the browser:" + shortcutKey
+				);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
